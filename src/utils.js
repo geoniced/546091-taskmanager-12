f0658d19db2de214d92fdb7f45eea53bd7bcf2f3
Ivan Kashin <barbos1701@gmail.com>
2020-08-09 07:59:44 +0300

    Добавляет моки фильтров и учит шаблон работать с данными фильтра

--- src/utils.js
+++ src/utils.js
@@ -5,19 +5,35 @@ export const getRandomInteger = (a = 0, b = 1) => {
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
+const getCurrentDate = () => {
+  const currentDate = new Date();
+  currentDate.setHours(23, 59, 59, 999);
+
+  return currentDate;
+};
+
 export const isTaskExpired = (dueDate) => {
   if (dueDate === null) {
     return false;
   }
 
-  let currentDate = new Date();
-  currentDate.setHours(23, 59, 59, 999);
+  const currentDate = getCurrentDate();
 
   return currentDate.getTime() > dueDate.getTime();
 };
 
 export const isTaskRepeating = (repeating) => {
   return Object.values(repeating).some(Boolean);
+};
+
+export const isTaskExpiringToday = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  const currentDate = getCurrentDate();
+
+  return currentDate.getTime() === dueDate.getTime();
 };
 
 export const humanizeTaskDueDate = (dueDate) => {
