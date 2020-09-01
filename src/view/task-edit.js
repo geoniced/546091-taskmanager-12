1bfbfc5d7a88fb525a4c7a2c5fb9da17576df1d3
Ivan Kashin <barbos1701@gmail.com>
2020-09-01 12:00:51 +0300

    Продолжаю делать в лоб, дополнив функционал тогглом повторения, также закомментировал основную логику (понадобится для личного проекта)

--- src/view/task-edit.js
+++ src/view/task-edit.js
@@ -1,6 +1,7 @@
 import AbstractView from './abstract.js';
 import {COLORS} from '../const.js';
 import {isTaskExpired, isTaskRepeating, humanizeTaskDueDate} from '../utils/task.js';
+import {renderTemplate, RenderPosition} from '../utils/render.js';
 
 const BLANK_TASK = {
   color: COLORS[0],
@@ -38,13 +39,13 @@ const createTaskEditDateTemplate = (dueDate, isDueDate) => {
   );
 };
 
-const createTaskEditRepeatingTemplate = (repeating) => {
+const createTaskEditRepeatingTemplate = (repeating, isRepeating) => {
   return (
     `<button class="card__repeat-toggle" type="button">
-      repeat:<span class="card__repeat-status">${isTaskRepeating(repeating) ? `yes` : `no`}</span>
+      repeat:<span class="card__repeat-status">${isRepeating ? `yes` : `no`}</span>
     </button>
 
-    ${isTaskRepeating(repeating) ? `<fieldset class="card__repeat-days">
+    ${isRepeating ? `<fieldset class="card__repeat-days">
       <div class="card__repeat-days-inner">
         ${Object.entries(repeating).map(([day, repeat]) => `<input
           class="visually-hidden card__repeat-day-input"
@@ -81,17 +82,17 @@ const createTaskEditColorsTemplate = (currentColor) => {
 
 const createTaskEditTemplate = (task, option) => {
   const {color, description, dueDate, repeating} = task;
-  const {isDueDate} = option;
+  const {isDueDate, isRepeating} = option;
 
   const deadlineClassName = isTaskExpired(dueDate)
     ? `card--deadline`
     : ``;
   const dateTemplate = createTaskEditDateTemplate(dueDate, isDueDate);
 
-  const repeatingClassName = isTaskRepeating(repeating)
+  const repeatingClassName = isRepeating
     ? `card--repeat`
     : ``;
-  const repeatingTemplate = createTaskEditRepeatingTemplate(repeating);
+  const repeatingTemplate = createTaskEditRepeatingTemplate(repeating, isRepeating);
 
   const colorsTemplate = createTaskEditColorsTemplate(color);
 
@@ -148,18 +149,94 @@ export default class TaskEdit extends AbstractView {
     this._task = task || BLANK_TASK;
     this._option = {
       isDueDate: Boolean(this._task.dueDate),
+      isRepeating: isTaskRepeating(this._task.repeating),
     };
 
     this._formSubmitHandler = this._formSubmitHandler.bind(this);
+
     this._enableDueDateToggler();
+    this._enableRepeatingToggler();
   }
 
   getTemplate() {
     return createTaskEditTemplate(this._task, this._option);
   }
 
-  _enableDueDateToggler() {
+  /*
+    Схема активации работы тогглеров:
+      1. Получаем элемент
+      2. Определяем функцию-хендлер
+        1) Удаляем связанные с тогглером элементы из разметки
+        2) Создаем новый шаблон по результату события
+          * Так как тоггл – смена значения на противоположное
+        3) Рендерим новый шаблон вместо старого
+        4) Меняем уже значение этой опции тогглера
+        5) Вешаем обработчик на новый отрендеренный элемент
+      3. Вешаем обработчик на элемент
+  */
 
+  _enableDueDateToggler() {
+    const element = this.getElement();
+
+    const dueDateToggleHandler = (evt) => {
+      evt.preventDefault();
+
+      // Стираем разметку, которая была
+      element.querySelector(`.card__date-deadline-toggle`).remove();
+      if (element.querySelector(`.card__date-deadline`)) {
+        element.querySelector(`.card__date-deadline`).remove();
+      }
+
+      // Создаем новую, уже с результатом нажатия по кнопке (противоположное значение – то что мы хотим достичь кликом)
+      const dateTemplate = createTaskEditDateTemplate(this._task.dueDate, !this._option.isDueDate);
+      renderTemplate(element.querySelector(`.card__dates`), dateTemplate, RenderPosition.AFTERBEGIN);
+
+      // Устанавливаем новые данные
+      this._option.isDueDate = !this._option.isDueDate;
+
+      // Добавляем обработчик на уже новую разметку, обработчик будет делать то же самое
+      element
+        .querySelector(`.card__date-deadline-toggle`)
+        .addEventListener(`click`, dueDateToggleHandler);
+    };
+
+    // Вешаем обработчик
+    element
+      .querySelector(`.card__date-deadline-toggle`)
+      .addEventListener(`click`, dueDateToggleHandler);
+  }
+
+  _enableRepeatingToggler() {
+    const element = this.getElement();
+
+    const repeatingToggleHandler = (evt) => {
+      evt.preventDefault();
+
+      element.querySelector(`.card__repeat-toggle`).remove();
+      if (element.querySelector(`.card__repeat-days`)) {
+        element.querySelector(`.card__repeat-days`).remove();
+      }
+
+      // При isRepeating === true, мы удаляем класс потому что мы делаем карточку от "обратных" свойств, что будет видно дальше
+      if (!this._option.isRepeating) {
+        element.classList.add(`card--repeat`);
+      } else {
+        element.classList.remove(`card--repeat`);
+      }
+
+      const repeatingTemplate = createTaskEditRepeatingTemplate(this._task.repeating, !this._option.isRepeating);
+      renderTemplate(element.querySelector(`.card__dates`), repeatingTemplate, RenderPosition.BEFOREEND);
+
+      this._option.isRepeating = !this._option.isRepeating;
+
+      element
+        .querySelector(`.card__repeat-toggle`)
+        .addEventListener(`click`, repeatingToggleHandler);
+    };
+
+    element
+      .querySelector(`.card__repeat-toggle`)
+      .addEventListener(`click`, repeatingToggleHandler);
   }
 
   _formSubmitHandler(evt) {
