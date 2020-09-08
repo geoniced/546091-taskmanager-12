fc45e9ab2e73b13ba07f54a84f33c016ee154217
Ivan Kashin <barbos1701@gmail.com>
2020-09-08 18:28:28 +0300

    Делает модель для фильтра, удаляет моки фильтра и учим представление работе фильтрации

--- src/main.js
+++ src/main.js
@@ -1,18 +1,26 @@
 import SiteMenuView from './view/site-menu.js';
 import FilterView from './view/filter.js';
 import {generateTask} from './mock/task.js';
-import {generateFilter} from './mock/filter.js';
 import BoardPresenter from './presenter/board.js';
 import TasksModel from './model/tasks.js';
+import FilterModel from './model/filter.js';
 import {render, RenderPosition} from './utils/render.js';
 
 const TASK_COUNT = 22;
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
-const filters = generateFilter(tasks);
+const filters = [
+  {
+    type: `all`,
+    name: `ALL`,
+    count: 0
+  }
+];
 
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
+
+const filterModel = new FilterModel();
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
@@ -20,6 +28,6 @@ const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
 
 render(siteHeaderElement, new SiteMenuView(), RenderPosition.BEFOREEND);
-render(siteMainElement, new FilterView(filters), RenderPosition.BEFOREEND);
+render(siteMainElement, new FilterView(filters, `all`), RenderPosition.BEFOREEND);
 
 boardPresenter.init();
