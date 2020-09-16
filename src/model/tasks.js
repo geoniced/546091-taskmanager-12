af734aa454c5d0c6cdec778d64e8d10b74bd8518
Ivan Kashin <barbos1701@gmail.com>
2020-09-16 17:49:20 +0300

    Реализует адаптер

--- src/model/tasks.js
+++ src/model/tasks.js
@@ -53,4 +53,44 @@ export default class Tasks extends Observer {
 
     this._notify(updateType);
   }
+
+  static adaptToClient(task) {
+    const adaptedTask = Object.assign(
+        {},
+        task,
+        {
+          dueDate: task.due_date !== null ? new Date(task.due_date) : task.due_date,
+          isArchive: task.is_archived,
+          isFavorite: task.is_favorite,
+          repeating: task.repeating_days
+        }
+    );
+
+    delete task.due_date;
+    delete task.is_archived;
+    delete task.is_favorite;
+    delete task.repeating_days;
+
+    return adaptedTask;
+  }
+
+  static adaptToServer(task) {
+    const adaptedTask = Object.assign(
+        {},
+        task,
+        {
+          "due_date": task.dueDate instanceof Date ? task.dueDate.toISOString() : null,
+          "is_archived": task.isArchive,
+          "is_favorite": task.isFavorite,
+          "repeating_days": task.repeating
+        }
+    );
+
+    delete task.dueDate;
+    delete task.isArchive;
+    delete task.isFavorite;
+    delete task.repeating;
+
+    return adaptedTask;
+  }
 }
